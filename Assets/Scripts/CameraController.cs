346b5f64168bc8584bf3f1a2f02e7da129590b27
Matt N <northmatt1@gmail.com>
2021-09-24 19:33:26 -0400

    Project Upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform following;
+    public Vector3 orginOffset;
+    public Vector2 clipPlaneOffset;
+    public float distance;
+    public float sensitivity;
+    public Vector2 rotXMinMax;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start() {
+        cam = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // LateUpdate is called once per frame at the end of everything
+    void LateUpdate() {
+        //Debug.Log(transform.rotation.eulerAngles.x - Input.GetAxis("Mouse Y") * sensitivity);
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if (Cursor.visible) {
+            if (Input.GetMouseButton(0)) {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+        else {
+            float rotMod = transform.rotation.eulerAngles.x <= 90 ? transform.rotation.eulerAngles.x + 360 : transform.rotation.eulerAngles.x;
+            transform.rotation = Quaternion.Euler(Mathf.Clamp(rotMod - Input.GetAxis("Mouse Y") * sensitivity, rotXMinMax.x, rotXMinMax.y),
+                transform.rotation.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivity, 0);
+        }
+
+        //Info provided is Camera FoV/Aspect Ratio/Near Clip Plane, get topleft of Near Clip Plane's local position in rectagular cordinates
+        float calc1 = Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad) * cam.nearClipPlane + clipPlaneOffset.x;    //see if this can be called only if smth changes to save on calcs
+        float calc2 = calc1 * cam.aspect + clipPlaneOffset.x;
+        float calc3 = cam.nearClipPlane - clipPlaneOffset.y;
+        float calcDist = distance;
+        //Vector3 calVec = new Vector3(calc2, calc1, calc3);
+        //Vector3 cenOfst = new Vector3(0.1f, 0.1f, 0.1f);
+        int layer = ~(1 << LayerMask.NameToLayer("Player"));
+        RaycastHit ray;
+
+        //follow.pos + orgOfst + Vec(0.1 * Pos/Neg(orgOfst.x), 0.1 * Pos/Neg(orgOfst.y), 0)     Pos/Neg() uses bitwise to find signed (but probs not cuz float)
+		//follow.pos + orgOfst + Vec(0.1 * vec.x, 0.1 * vec.y, 0)	vec is (1, 1, 1)
+        //getting Hypotnuse dist
+        foreach (Vector3 vec in new Vector3[4] { new Vector3(calc2, calc1, calc3), new Vector3(-calc2, calc1, calc3), new Vector3(calc2, -calc1, calc3), new Vector3(-calc2, -calc1, calc3) })
+            if (Physics.Linecast(following.position + orginOffset, following.position + orginOffset + transform.rotation * (Vector3.back * distance + vec), out ray, layer) && ray.distance < calcDist)
+                calcDist = ray.distance;
+
+        following.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+        transform.position = following.position + orginOffset - transform.forward * (Mathf.Cos(cam.fieldOfView * 0.5f * Mathf.Deg2Rad) * calcDist);
+    }
+}
---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+struct Inputs {
+    public Vector2 axis;
+    public Vector2 tempAxis;
+    public uint framesPassed;
+    public byte jump;
+}
+
+public class CharController : MonoBehaviour {
+    public float moveForce;
+    public float jumpForce;
+    public float gravMult;
+
+    private Inputs curInputs;
+    private Rigidbody RB3D;
+    private int grounds = 0;
+    private float sqrt2 = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        RB3D = gameObject.GetComponent<Rigidbody>();
+        sqrt2 = 1f / Mathf.Sqrt(2);         //sqrt is a fairly intensive operation, storing it in memory to avoid using opertaion every fixed update
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Cursor.visible)
+            return;
+
+        //Store input from each update to be considered for fixed updates, dont do needless addition of 0 if unneeded
+        curInputs.tempAxis.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (curInputs.tempAxis.x != 0f) curInputs.axis.x += curInputs.tempAxis.x;
+        if (curInputs.tempAxis.y != 0f) curInputs.axis.y += curInputs.tempAxis.y;
+        ++curInputs.framesPassed;
+        if (Input.GetButtonDown("Jump") && curInputs.jump == 0 && grounds > 0) curInputs.jump = 5;
+    }
+
+    private void FixedUpdate() {
+        //if both inputs were pressed then normalize inputs
+        if (curInputs.axis.x != 0f && curInputs.axis.y != 0f)
+            curInputs.axis.Set(curInputs.axis.x * sqrt2, curInputs.axis.y * sqrt2);
+
+        if (curInputs.axis.x != 0f) {
+            RB3D.AddForce(transform.right * moveForce * curInputs.axis.x / curInputs.framesPassed * Time.fixedDeltaTime);
+            curInputs.axis.x = 0f;
+        }
+
+        if (curInputs.axis.y != 0f) {
+            RB3D.AddForce(transform.forward * moveForce * curInputs.axis.y / curInputs.framesPassed * Time.fixedDeltaTime);
+            curInputs.axis.y = 0f;
+        }
+
+        //jump on maxed cooldown
+        if (curInputs.jump == 5)
+            RB3D.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        
+        //reduce jump cooldown if grounded
+        if (curInputs.jump > 0 && grounds > 0)
+            --curInputs.jump;
+
+        //Add downwards force if ungrounded (RB3D has drag)
+        if (grounds <= 0)
+            RB3D.AddForce(Physics.gravity * gravMult, ForceMode.Acceleration);
+
+        curInputs.framesPassed = 0;
+    }
+
+    //TODO: add/remove col references to dynamic array for checking each active collision if their hit normal is at a threshhold for grounded when needed (function called by jumping)
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground"))
+            ++grounds;
+    }
+    private void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground"))
+            --grounds;
+    }
+}
